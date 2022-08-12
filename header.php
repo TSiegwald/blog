918e92bdbf07c909a062e8fee76136d4e4da823c
Thomas Siegwald <siegwald.thomas@gmail.com>
2022-08-12 09:45:45 +0200

    Créez un template de page

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Le blog de l'AVBN</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+
+    <body>
